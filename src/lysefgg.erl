093032c6a391ca2a65b8ed73988e09d5b8f8ff79
Neftaly Luis <neftaly.luis@resuelve.mx>
2019-03-05 21:29:53 -0600

    Primeras funciones

--- src/lysefgg.erl
+++ src/lysefgg.erl
@@ -1,13 +1,20 @@
 -module(lysefgg).
 
 %% API exports
--export([]).
+-export([tail_len/1, tail_duplicate/2]).
 
 %%====================================================================
 %% API functions
 %%====================================================================
+tail_len(L) -> tail_len(L, 0).
+tail_duplicate(N, Term) -> tail_duplicate(N, Term, []).
 
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+tail_len([], Acc) -> Acc;
+tail_len([_ | T], Acc) -> tail_len(T, Acc + 1).
+
+tail_duplicate(0, _, List) -> List;
+tail_duplicate(N, Term, List) when N > 0 -> tail_duplicate(N - 1, Term, [Term, List]).
